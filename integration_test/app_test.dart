388d0184f6b863362da044245160176797a04dc0
jonasjakobsen1152 <114471902+jonasjakobsen1152@users.noreply.github.com>
2024-03-01 08:00:15 +0100

    save

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 // Replace with your app name
@@ -9,6 +10,44 @@ void main() {
   testWidgets('Test MyApp', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
-    // TODO write your test code here
+    testWidgets('Test Calculator Operations', (WidgetTester tester) async {
+      await tester.pumpWidget(MyApp());
+
+      // Find buttons
+      var button2Finder = find.widgetWithText(ElevatedButton, '2');
+      var button3Finder = find.widgetWithText(ElevatedButton, '3');
+      var buttonPlusFinder = find.widgetWithText(ElevatedButton, '+');
+      var buttonEnterFinder = find.widgetWithText(ElevatedButton, 'enter');
+
+      // Tap '2'
+      await tester.tap(button2Finder);
+      await tester.pumpAndSettle();
+
+      // Tap 'enter'
+      await tester.tap(buttonEnterFinder);
+      await tester.pumpAndSettle();
+
+      // Tap '3'
+      await tester.tap(button3Finder);
+      await tester.pumpAndSettle();
+
+      // Tap 'enter'
+      await tester.tap(buttonEnterFinder);
+      await tester.pumpAndSettle();
+
+      // Tap '+'
+      await tester.tap(buttonPlusFinder);
+      await tester.pumpAndSettle();
+
+      // Tap 'enter'
+      await tester.tap(buttonEnterFinder);
+      await tester.pumpAndSettle();
+
+      // Find the result text
+      var resultFinder = find.text('5');
+
+      // Verify the result is '5'
+      expect(resultFinder, findsOneWidget);
+    });
   });
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rpn_calculator/RPNCalculator.dart';
+import 'package:rpn_calculator/rpn_calculator.dart';
 
 void main() {
   runApp(MyApp());
